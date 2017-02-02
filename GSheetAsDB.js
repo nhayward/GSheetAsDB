7e46716014a0fb324202ad5fb447dfd7272679f8
Nick Hayward <nhayward@magnesium.local>
2017-02-02 00:05:14 -0500

    supporting the use of multiple sheets in an application

--- GSheetAsDB.js
+++ GSheetAsDB.js
@@ -13,55 +13,68 @@
  * (https://github.com/nhayward/GSheetAsDB/blob/master/LICENSE)
  */
 
-function initSheetData() {
-	// check for optional "tab" key in sheet object and default to 1 if not found
-	var tab = sheet.tab ? sheet.tab : 1;
-	// the following sheet URL structure will return JSON from a publicly published ghseet at the specified tab number
-	var sheetURL = 'https://spreadsheets.google.com/feeds/list/' + sheet.id + '/' + tab + '/public/values?alt=json';
-	$.getJSON(sheetURL, function(data) {
-		var sheetData = [];
-		var rowData = {};
-		$.each(data.feed.entry, function() {
-			// iterate through each entry object's keys
-			$.each($(this)[0], function(key, value) {
-				// keys relevant to columns will all start with "gsx$" 
-				// followed by the column name in lowercase without spaces
-				// ex: column "Test 1" will show up as "gsx$test1"
-				if (key.startsWith("gsx$")) {
-					// add key to rowData object without the leading "gsx$"
-					// the value of the cell is stored as "$t" within the value of the "gsx$" key
-					rowData[key.substring(4)] = value.$t;
-				}
+function initSheetData(sheets) {
+	var completionCount = 0;
+	sheets.forEach(function(sheetToInit, i) {
+		// check for optional "tab" key in sheet object and default to 1 if not found
+		var tab = sheetToInit.tab ? sheetToInit.tab : 1;
+		// the following sheet URL structure will return JSON from a publicly published ghseet at the specified tab number
+		var sheetURL = 'https://spreadsheets.google.com/feeds/list/' + sheetToInit.id + '/' + tab + '/public/values?alt=json';
+		$.getJSON(sheetURL, function(data) {
+			var sheetData = [];
+			var rowData = {};
+			$.each(data.feed.entry, function() {
+				// iterate through each entry object's keys
+				$.each($(this)[0], function(key, value) {
+					// keys relevant to columns will all start with "gsx$" 
+					// followed by the column name in lowercase without spaces
+					// ex: column "Test 1" will show up as "gsx$test1"
+					if (key.startsWith("gsx$")) {
+						// add key to rowData object without the leading "gsx$"
+						// the value of the cell is stored as "$t" within the value of the "gsx$" key
+						rowData[key.substring(4)] = value.$t;
+					}
+				});
+				// push rowData to the sheetData array and reset the rowData object
+				sheetData.push(rowData);
+				rowData = {};
 			});
-			// push rowData to the sheetData array and reset the rowData object
-			sheetData.push(rowData);
-			rowData = {};
+			// set the sheetData array as the value of the "data" key in the sheet object
+			sheetToInit.data = sheetData;
+		}).always(function() {
+			completionCount++;
+			if (completionCount === sheets.length) {
+				// allow any further JavaScript that is within $(document).ready(...) to execute now
+				$.holdReady(false);
+			}
 		});
-		// set the sheetData array as the value of the "data" key in the sheet object
-		sheet.data = sheetData;
-	}).always(function() {
-		// allow any further JavaScript that is within $(document).ready(...) to execute now
-		$.holdReady(false);
 	});
 }
 
-function postDataToSheet(data) {
+function postDataToSheet(data, sheetToPost, update) {
 	// post to your sheet using the sheet's Google script
 	$.post(
-		sheet.postScriptURL,
+		sheetToPost.postScriptURL,
 		data // creates a new row with the data you provide here
 	);
-	// now we will update the sheet.data object on the front end
-	// 
-	// the data keys must be massaged to look like they do when
-	// spreadsheet data is returned
-	for (var key in data) {
-		// lowercase the key and remove anything non-alphanumeric
-		var newKey = key.toLowerCase().replace(/[^a-zA-Z0-9]/gi, "");
-		data[newKey] = data[key];
-		// remove the old key
-		delete data[key];
+	// check if update has been explicitly passed, default to true
+	if (typeof(update) === "undefined") {
+		update = true;
 	}
-	// update the data object on the front end
-	sheet.data.push(data);
+	// only update the data object on the front end if update is true
+	if (update) {
+		// now we will update the sheet data object on the front end
+		// 
+		// the data keys must be massaged to look like they do when
+		// spreadsheet data is returned
+		for (var key in data) {
+			// lowercase the key and remove anything non-alphanumeric
+			var newKey = key.toLowerCase().replace(/[^a-zA-Z0-9]/gi, "");
+			data[newKey] = data[key];
+			// remove the old key
+			delete data[key];
+		}
+		// update the data object on the front end
+		sheetToPost.data.push(data);
+	}
 }
